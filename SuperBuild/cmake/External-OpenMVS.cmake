e05b588d960bfdd33e327196669978f1b30091f1
oradzhabov <oradzhabov@gmail.com>
2021-03-29 11:28:33 +0300

    ODM v2.4.1 referenced to OpenMVS(240).optim-ros

--- SuperBuild/cmake/External-OpenMVS.cmake
+++ SuperBuild/cmake/External-OpenMVS.cmake
@@ -19,8 +19,8 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  GIT_REPOSITORY    https://github.com/OpenDroneMap/openMVS
-  GIT_TAG           240
+  GIT_REPOSITORY    https://github.com/oradzhabov/openMVS.git
+  GIT_TAG           optim-ros
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
